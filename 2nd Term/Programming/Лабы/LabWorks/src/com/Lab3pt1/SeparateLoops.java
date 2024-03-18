bb0e1851905b1cb154ddb7db48faf1ee9e724bd7
Cindora <st111245@student.spbu.ru>
2024-03-18 20:11:01 +0300

    add Lab3.1 solution

---
+++ 2nd Term/Programming/Лабы/LabWorks/src/com/Lab3pt1/SeparateLoops.java
@@ -0,0 +1,77 @@
+package com.Lab3pt1;
+
+import java.util.Scanner;
+
+public class SeparateLoops {
+    /**
+     * Алгоритм, вычисляющий значение выражения при помощи вложенных циклов по входящим параметрам.
+     */
+    public static double solveEquationFor(int N, int M, double a) {
+        double result = 0;
+
+        for (int i = 1; i <= N; i++) {
+            result += (i+a)/(2*i+1);
+            result += (i+a)/(a*i);
+        }
+        for (int k = 1; k <= M; k++) {
+            result += (double) (2 * k) / (2+k);
+        }
+
+        return result;
+    }
+
+    public static double solveEquationWhile(int N, int M, double a) {
+        double result = 0;
+
+        int i = 1;
+        while (i <= N) {
+            result += (i+a)/(2*i+1);
+            result += (i+a)/(a*i);
+            i++;
+        }
+
+        int k = 1;
+        while (k <= M) {
+            result += (double) (2 * k) / (2+k);
+            k++;
+        }
+
+        return result;
+    }
+
+    public static double solveEquationDoWhile(int N, int M, double a) {
+        double result = 0;
+
+        if (N>=1) {
+            int i = 1;
+            do {
+                result += (i+a)/(2*i+1);
+                result += (i+a)/(a*i);
+                i++;
+            } while (i <= N);
+        }
+
+        if (M>=1) {
+            int k = 1;
+            do {
+                result += (double) (2 * k) / (2+k);
+                k++;
+            } while (k <= M);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt(),
+                M = in.nextInt();
+        double a = in.nextDouble();
+
+        double  answerFor = solveEquationFor(N, M, a),
+                answerWhile = solveEquationWhile(N, M, a),
+                answerDoWhile = solveEquationDoWhile(N, M, a);
+        System.out.println("Answer (for): " + answerFor +
+                "\nAnswer (while): " + answerWhile +
+                "\nAnswer (do while): " + answerDoWhile);
+    }
+}
