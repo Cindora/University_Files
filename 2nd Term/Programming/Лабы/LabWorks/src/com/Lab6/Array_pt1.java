ffb52cff73aceece6ae981681c3b908fc2c72102
Cindora <st111245@student.spbu.ru>
2024-04-12 13:32:10 +0300

    add Programming Lab6pt1 solution

---
+++ 2nd Term/Programming/Лабы/LabWorks/src/com/Lab6/Array_pt1.java
@@ -0,0 +1,45 @@
+package com.Lab6;
+import java.util.Scanner;
+
+public class Array_pt1 {
+    public static void main(String[] args) {
+        // Ввод массива
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество элементов массива: ");
+        int n = scanner.nextInt();
+        int[] Xn = new int[n];
+
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < n; i++) {
+            Xn[i] = scanner.nextInt();
+        }
+        //
+
+        // Вычислеие среднего значения
+        double sum = 0;
+        for (int el : Xn) {
+            sum += el;
+        }
+        double avg = sum / Xn.length;
+        //
+
+        // Количество элементов, меньших среднего
+        int countLessThanAvg = 0;
+        for (int el : Xn) {
+            if (el < avg) {
+                countLessThanAvg++;
+            }
+        }
+        System.out.println("Количество элементов массива, меньших среднего: " + countLessThanAvg);
+        //
+
+        // Значения и номера элементов, делящихся на 3
+        System.out.println("Элементы, делящиеся на 3 без остатка:");
+        for (int i = 0; i < Xn.length; i++) {
+            if (Xn[i] % 3 == 0) {
+                System.out.println("Элемент: " + Xn[i] + ". Номер: " + i);
+            }
+        }
+        //
+    }
+}
