118092b706d28ad2eff8302c9fa8ef570897678f
Lenovo Legion 5 <pellegac@gmail.com>
2025-12-06 17:27:48 +0300

    06.10.2025 17:00

---
+++ 5th Term/Information systems and DB/Lab3pt1/Lab3_1/FormBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_1
+{
+    public partial class FormBook : Form
+    {
+        public FormBook()
+        {
+            InitializeComponent();
+        }
+
+        private void FormBook_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "dataSetClass._class". При необходимости она может быть перемещена или удалена.
+            this.classTableAdapter.Fill(this.dataSetClass._class);
+
+        }
+    }
+}
